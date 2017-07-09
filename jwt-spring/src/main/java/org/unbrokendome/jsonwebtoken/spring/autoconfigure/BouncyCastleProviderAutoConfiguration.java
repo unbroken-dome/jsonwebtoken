77a1268e46b7a4c251c1f70c00ffdf695e8eb419
Till Krullmann <till.krullmann@gmx.net>
2017-07-09 15:21:05 +0200

    Fix BouncyCastleProviderAutoConfiguration:
    - use static initializer to make sure the provider is installed early on
    - add auto-configuration to META-INF/spring.factories

--- jwt-spring/src/main/java/org/unbrokendome/jsonwebtoken/spring/autoconfigure/BouncyCastleProviderAutoConfiguration.java
+++ jwt-spring/src/main/java/org/unbrokendome/jsonwebtoken/spring/autoconfigure/BouncyCastleProviderAutoConfiguration.java
@@ -1,6 +1,7 @@
 package org.unbrokendome.jsonwebtoken.spring.autoconfigure;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +10,12 @@ import java.security.Security;
 
 @Configuration
 @ConditionalOnClass(name = "org.bouncycastle.jce.provider.BouncyCastleProvider")
+@AutoConfigureBefore({
+        JwtAutoConfiguration.class
+})
 public class BouncyCastleProviderAutoConfiguration {
 
-    public BouncyCastleProviderAutoConfiguration() {
+    static {
         if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
             Security.addProvider(new BouncyCastleProvider());
         }
