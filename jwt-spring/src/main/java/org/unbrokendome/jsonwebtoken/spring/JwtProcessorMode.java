eb473ef111114553daf9334da1d7286358d976ba
Till Krullmann <till.krullmann@gmx.net>
2018-09-18 21:23:03 +0200

    added Javadocs

--- jwt-spring/src/main/java/org/unbrokendome/jsonwebtoken/spring/JwtProcessorMode.java
+++ jwt-spring/src/main/java/org/unbrokendome/jsonwebtoken/spring/JwtProcessorMode.java
@@ -1,7 +1,20 @@
 package org.unbrokendome.jsonwebtoken.spring;
 
 public enum JwtProcessorMode {
+
+    /**
+     * The processor should support full JWT operations (both encoding and decoding).
+     */
     FULL,
+
+    /**
+     * The processor should only support encoding of claims into JWTs, including cryptographic signing.
+     */
     ENCODE_ONLY,
+
+    /**
+     * The processor should only support decoding of JWTs into claims,
+     * including verification of cryptographic signatures.
+     */
     DECODE_ONLY
 }
